a0d926dec20d89da3aa132e9dd9a7197d015292e
“Chris <hh0975832701@gmail.com>
2019-09-20 22:35:59 +0800

    Initial Commit

---
+++ grotion_hw1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  grotion_hw1
+//
+//  Created by User24 on 2019/9/20.
+//  Copyright © 2019 User24. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
